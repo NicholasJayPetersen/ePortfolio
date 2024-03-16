b54a94b2c91a5010cf7f30222be87f009a42192a
hugotomic <nicholas.jay.petersen@gmail.com>
2024-03-15 23:23:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -37,6 +37,9 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7960643373169670"
+     crossorigin="anonymous"></script>
+
 </head>
 <body class="host_version"> 
 
